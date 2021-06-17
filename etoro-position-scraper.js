46e293c8f6a4bd6566784ecafe42edec9a85f498
Dominik Baron <dominik.b4ron@gmail.com>
2021-06-17 18:25:05 +0200

    Improve etoro position scraper

--- etoro-position-scraper.js
+++ etoro-position-scraper.js
@@ -1,15 +1,19 @@
 delete positions;
 positions = [];
 
+function polish(str) {
+    return str.trim().replace('$', '');
+}
+
 document.querySelector('*[data-etoro-automation-id="portfolio-manual-trades-table-body-container"]')
     .children
     .forEach(c => {
         const isBuy = c.querySelector('*[data-etoro-automation-id="portfolio-manual-trades-table-body-market-buy-label"]')?.innerHTML.trim().toLowerCase() === 'buy';
-        const symbol = c.querySelector('*[data-etoro-automation-id="portfolio-manual-trades-table-body-market-name"]').innerHTML.trim();
-        const invested = c.querySelector('*[data-etoro-automation-id="portfolio-manual-trades-table-body-cell-container-invested"] > span').innerHTML.trim();
-        const open = c.querySelector('*[data-etoro-automation-id="portfolio-manual-trades-table-body-cell-container-open-rate"] > span').innerHTML.trim();
-        const leverage = c.querySelector('*[data-etoro-automation-id="portfolio-manual-trades-table-body-cell-container-leverage"] > span').innerHTML.trim();
-        const fees = c.querySelector('*[data-etoro-automation-id="portfolio-manual-trades-table-body-cell-container-fees"] > span').innerHTML.trim();
+        const symbol = polish(c.querySelector('*[data-etoro-automation-id="portfolio-manual-trades-table-body-market-name"]').innerHTML);
+        const invested = polish(c.querySelector('*[data-etoro-automation-id="portfolio-manual-trades-table-body-cell-container-invested"] > span').innerHTML);
+        const open = polish(c.querySelector('*[data-etoro-automation-id="portfolio-manual-trades-table-body-cell-container-open-rate"] > span').innerHTML);
+        const leverage = polish(c.querySelector('*[data-etoro-automation-id="portfolio-manual-trades-table-body-cell-container-leverage"] > span').innerHTML);
+        const fees = polish(c.querySelector('*[data-etoro-automation-id="portfolio-manual-trades-table-body-cell-container-fees"] > span').innerHTML);
 
         positions.push({
             symbol,
